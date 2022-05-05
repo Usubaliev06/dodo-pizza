b80a2385cadc8af44ce7bc6f81184aac0544ee85
adil <adilusubaliev@mail.ru>
2022-05-05 18:06:28 +0600

    get add delete

--- src/api/Api.js
+++ src/api/Api.js
@@ -2,21 +2,21 @@ import axios from "axios";
 import { baseUrl, pizzaApi } from "../constants/api";
 
 const http = axios.create({
-    baseURL :baseUrl
+    baseURL: baseUrl
 })
 
 const api = axios.create({
-    baseURL:"http://solid.lol/"
+    baseURL: "http://solid.lol/"
 })
 
-api.interceptors.request.use((config)=>{
-config.headers.token=JSON.parse(localStorage.getItem("auth"))?.token
-return config
+api.interceptors.request.use((config) => {
+    config.headers.token = JSON.parse(localStorage.getItem("auth"))?.token
+    return config
 })
 
 export default {
     getPizzas: () => api.get("getall/pizza"),
-    craetePizza: (data) => api.post("add/pizza" ,data ),
-    auth:(data) => api.post("admin", data),
-
+    craetePizza: (data) => api.post("add/pizza", data),
+    auth: (data) => api.post("admin", data),
+    delete:(id) => api.delete("delete/pizza" + id)
 }
